123026d71fd144f0dc7532873e2fe677bdf40bed
wxmaudio <wxm224@163.com>
2016-06-28 15:58:32 +0800

    Add Charactor component router

--- app/components/Character.js
+++ app/components/Character.js
@@ -26,6 +26,13 @@ class Character extends React.Component{
 		  });
 	}
 
+	componentDidUpdate(prevProps){
+		 // Fetch new charachter data when URL path WXMpanguso1?
+		 // 
+		if(prevProps.params.id !== this.props.params.id){
+			CharacterActions.getCharacter(this.props.params.id);
+		}
+	}
 	componentWillUnmout(){
 		CharacterStore.unlisten(onChange);
 		$(document.body).removeClass();
@@ -36,6 +43,31 @@ class Character extends React.Component{
 	}
 
 	render(){
-
+		 <div className='container'>
+        <div className='profile-img'>
+          <a className='magnific-popup' href={'https://image.eveonline.com/Character/' + this.state.characterId + '_1024.jpg'}>
+            <img src={'https://image.eveonline.com/Character/' + this.state.characterId + '_256.jpg'} />
+          </a>
+        </div>
+        <div className='profile-info clearfix'>
+          <h2><strong>{this.state.name}</strong></h2>
+          <h4 className='lead'>Race: <strong>{this.state.race}</strong></h4>
+          <h4 className='lead'>Bloodline: <strong>{this.state.bloodline}</strong></h4>
+          <h4 className='lead'>Gender: <strong>{this.state.gender}</strong></h4>
+          <button className='btn btn-transparent'
+                  onClick={CharacterActions.report.bind(this, this.state.characterId)}
+                  disabled={this.state.isReported}>
+            {this.state.isReported ? 'Reported' : 'Report Character'}
+          </button>
+        </div>
+        <div className='profile-stats clearfix'>
+          <ul>
+            <li><span className='stats-number'>{this.state.winLossRatio}</span>Winning Percentage</li>
+            <li><span className='stats-number'>{this.state.wins}</span> Wins</li>
+            <li><span className='stats-number'>{this.state.losses}</span> Losses</li>
+          </ul>
+        </div>
+      </div>
 	}
 }
+Character.propType={};
